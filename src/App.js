ea4d664ca7ff9ec8dd52957bcc3bfd4bdf5e98ba
Jamie Gaehring <gaehj457@gmail.com>
2023-03-11 16:34:28 -0500

    More arbitrary changes.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <Dashboard name="Blackbird"/>
-      <h2>Start editing to see some magic happen!</h2>
+      <Dashboard name="Blackbird">
+        <h2>Start editing to see some magic happen!</h2>
+      </Dashboard>
     </div>
   );
 }
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,3 +1,3 @@
 export default function Dashboard(props) {
-  return <div><h1>{props.name}!</h1></div>;
+  return <div><h1>{props.name}!</h1>{props.children}</div>;
 }
